a444a9d071a8eaa719c24a80416fdceac33a4bd5
shakeeb-sa <shakeeb.sa.ahmed@gmail.com>
2025-11-01 17:15:37 +0500

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This is the line you need to add
+  base: "/portfolio/", 
+  
   plugins: [react()],
 })
