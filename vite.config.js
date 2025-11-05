ad98b411a1cb67984a9ca973bd29b09331176423
shakeeb-sa <shakeeb.sa.ahmed@gmail.com>
2025-11-05 14:50:19 +0500

    fix: correct base path in vite config for root domain

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This is the line you need to add
-  base: "/portfolio/", 
+  // BEFORE:
+  // base: "/portfolio/", 
   
+  // AFTER:
+  base: "/", 
+
   plugins: [react()],
 })
